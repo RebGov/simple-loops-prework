25173e09040f23f931828bde3ff5d365658f4b36
Rebecca Govert <becci.govert@gmail.com>
2018-05-23 14:36:45 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,10 +2,10 @@
 
 def loop_iterator(number_of_times)
   count = 0
-  
+  phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
     count += 1 
-    puts "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     if count == number_of_times
      break
    end
